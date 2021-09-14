d91cd4bfb0016bc5223ac98ed643c716f76bdda4
Joy LoRuth <46979334+joyloruth@users.noreply.github.com>
2021-09-14 16:12:25 +0000

    removed unused images

---
+++ script.js
@@ -0,0 +1,16 @@
+
+    function solve(value){
+      var crossword;
+
+        if(value == 0){
+         crossword = "crossword_puzzle/crossword.png";
+        }
+        else{
+          crossword = "crossword_puzzle/crossword.png";
+         
+        }
+
+        document.getElementById("crossword").src=lightswitch;
+
+        document.getElementById("outside").src=weather;
+    }
